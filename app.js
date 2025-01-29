87980d9a25af45e7120190e9937b32e52cd4d0e4
Dan-Gaya <kaniabdulaziznura@gmail.com>
2025-01-29 15:29:13 +0100

    update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const infoRoutes = require('./Routes/info_Route')
 
 app.use('/api',infoRoutes);
 app.use('/', (req, res,next) => {
-    res.send('Hello, welcome to Task amnagement API. URl:http://localhost:3000/api/ ');
+    res.send('Hello, welcome to HNG STAGE ZERO. visit https://stage-zero-lilac.vercel.app/api ');
   });
 
 //error handling
