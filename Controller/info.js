d9db44308e711cadeab978effc853eef99c3dfe7
Dan-Gaya <kaniabdulaziznura@gmail.com>
2025-01-29 21:34:59 +0100

    Update info.js

--- Controller/info.js
+++ Controller/info.js
@@ -1,17 +1,10 @@
 
 
 const DisplayInfo = (req,res)=>{
-const currentDate = new Date();
-
-    // Get the timezone offset in minutes and convert it to milliseconds
-    const timezoneOffset = currentDate.getTimezoneOffset() * 60000;
-
-    // Adjust the current date to local time
-    const localISODate = new Date(currentDate.getTime() - timezoneOffset).toISOString();
 
     const message = {
         email: 'abdlnurakani@gmail.com',
-        current_datetime: localISODate,
+        current_datetime: new Date().toISOString(),
         github_url: 'https://github.com/Dan-Gaya/STAGE_ZERO'
     };
 
